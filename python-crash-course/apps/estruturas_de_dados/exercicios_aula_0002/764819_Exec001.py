0ce8088b0ac62398953a085848050493a474e338
Jefferson <jefferson.mycontact@gmail.com>
2026-09-22 07:57:17 -0300

    Remove cheat_sheets e estruturas_de_dados do repositório remoto

--- python-crash-course/apps/estruturas_de_dados/exercicios_aula_0002/764819_Exec001.py
+++
@@ -1,7 +0,0 @@
-notas = [10.0, 9.5, 9.0, 8.5, 8.0, 7.5]
-
-print(f"Nota do primeiro aluno: {notas[0]}")
-
-print(f"Nota do terceiro aluno: {notas[2]}")
-
-print(f"Nota do último aluno: {notas[-1]}")
--- python-crash-course/apps/estruturas_de_dados/exercicios_aula_0002/764819_Exec004.py
+++
@@ -1,7 +0,0 @@
-base = 3000
-tamanho = 8
-indice = int(input("Digite o índice do vetor: "))
-
-endereco = base + indice * tamanho
-
-print(f"Endereço de uma posição do vetor: {endereco}")
